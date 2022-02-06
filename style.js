7dd9a42519ea51b7cbad29b7006f66e1d7bd496a
@cheeks cloud <agneskaswii700@gmail.com>
2022-02-06 11:20:16 +0300

    ft: function added a function with variables

--- style.js
+++ style.js
@@ -1,11 +1,11 @@
-// function check() { 
-//     var d = 0;
-//     var q1 = document.quiz.question1.value;
-//     var q2 = document.quiz.question2.value;
-//     var q3 = document.quiz.question3.value; 
-//     var q4 = document.quiz.question4.value;
-//     var q5 = document.quiz.question5.value;
-//     var q6 = document.quiz.question6.value;
+function check() { 
+    var d = 0;
+    var q1 = document.quiz.question1.value;
+    var q2 = document.quiz.question2.value;
+    var q3 = document.quiz.question3.value; 
+    var q4 = document.quiz.question4.value;
+    var q5 = document.quiz.question5.value;
+    var q6 = document.quiz.question6.value;
 
     
 
